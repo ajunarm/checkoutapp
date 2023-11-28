e4bc8d298e06d3aa46ab15c5cb7b20545e8f4a81
Bueze Nwokolo <ony19001@byui.edu>
2023-11-28 12:21:17 -0700

    changed database connection

--- ui/connectdb.php
+++ ui/connectdb.php
@@ -1,8 +1,28 @@
 <?php
 
+// $servername = "your_server_host";
+// $username = "your_username";
+// $password = "your_password";
+// $database = "your_database";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// echo "Connected to the database successfully!";
+
+// // Perform database operations here...
+
+// // Close the connection
+// $conn->close();
+
 try
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=checkoutapp_db', 'root', '');
+    $pdo = new PDO('mysql:host=tier2checkoutappserver.mysql.database.azure.com', 'shiftlead', 'Ch0ose T7e R!ght');
 }
 catch (PDOException $e)
 {
