87280789f29d8c045bf2d5f6d585679cc20801b6
Bueze Nwokolo <ony19001@byui.edu>
2023-11-28 12:55:06 -0700

    minor naming update

--- ui/connectdb.php
+++ ui/connectdb.php
@@ -24,7 +24,7 @@ try {
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::MYSQL_ATTR_SSL_CA      => "/var/www/html/DigiCertGlobalRootG2.crt.pem",
+        PDO::MYSQL_ATTR_SSL_CA       => "/var/www/html/DigiCertGlobalRootCA.crt.pem",
         // If you have a client certificate and key, include them as well:
         // PDO::MYSQL_ATTR_SSL_CERT    => "/path/to/client-cert.pem",
         // PDO::MYSQL_ATTR_SSL_KEY     => "/path/to/client-key.pem",
