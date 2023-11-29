9a857fbe2ab89ef0d2dab5a35e40819a6d56bbd1
Bueze Nwokolo <ony19001@byui.edu>
2023-11-29 01:49:38 -0700

    test10

--- index.php
+++ index.php
@@ -28,8 +28,8 @@ try
   $options = array(
     PDO::MYSQL_ATTR_SSL_CA => 'C:\Users\nwoko\OneDrive\Desktop\DigiCertGlobalRootCA.crt.pem'
   );
-  $pdo = new PDO('mysql:host=tier2checkoutappserver.mysql.database.azure.com;port=3306;dbname=tier2checkoutschema', 'shiftlead', 'Ch0ose T7e R!ght', $options);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo = new PDO('mysql:host=tier2checkoutappserver.mysql.database.azure.com;port=3306;dbname=tier2checkoutschema', 'shiftlead', 'Ch0ose T7e R!ght');
+  // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e)
 {
