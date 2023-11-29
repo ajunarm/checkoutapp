b7ee1fb51f2f2ac549d74c09dab1aaf7a5f23a81
daniel_eugene <ony19001@byui.edu>
2023-11-29 09:06:13 -0700

    update

--- ui/connectdb.php
+++ ui/connectdb.php
@@ -1,12 +1,8 @@
 <?php
 
-try 
+try
 {
-  $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => 'C:\Users\nwoko\OneDrive\Desktop\DigiCertGlobalRootCA.crt.pem'
-  );
   $pdo = new PDO('mysql:host=tier2checkoutappserver.mysql.database.azure.com;port=3306;dbname=tier2checkoutschema', 'shiftlead', 'Ch0ose T7e R!ght');
-  // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e)
 {
